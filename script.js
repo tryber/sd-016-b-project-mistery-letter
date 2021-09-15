af7471eb336ed5b560082e6e10657c9da7f6c348
Gabriel Pinheiro <g.felippe5965@gmail.com>
2021-09-15 11:43:49 -0300

    Exercício 4 e 5 - Concluído.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const getInput = document.getElementById('carta-texto');
 const getButton = document.getElementById('criar-carta');
+const getParagraph = document.getElementById('carta-gerada');
 
 // ref: https://www.samanthaming.com/tidbits/83-4-ways-to-convert-string-to-character-array/
 function createLetter() {
-  const getParagraph = document.getElementById('carta-gerada');
   const replaceSpace = getInput.value.split(' ');
   const arrayInput = Array.from(replaceSpace);
   for (let i = 0; i < arrayInput.length; i += 1) {
@@ -11,6 +11,13 @@ function createLetter() {
     createSpan.innerText = `${arrayInput[i]}`;
     getParagraph.appendChild(createSpan);
   }
+  emptyInput();
 }
 
 getButton.addEventListener('click', createLetter);
+
+function emptyInput() {
+  if (getInput.value === '' || getInput.value === ' ') {
+    getParagraph.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  }
+}
