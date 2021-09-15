b622d36f36327999445e9f5a3d6eedfec740158b
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-09-15 13:08:46 -0300

    Codigo Refatorado

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const inputText = document.getElementById('carta-texto')
-const text = document.getElementById('carta-gerada')
-const createLetterBtn = document.getElementById('criar-carta')
-const spans = document.getElementsByTagName('span')
+const inputText = document.getElementById('carta-texto');
+const text = document.getElementById('carta-gerada');
+const createLetterBtn = document.getElementById('criar-carta');
+const contadorOutput = document.getElementById('carta-contador');
+let contador = 0;
 
-function randomClass (grupoClasse) {
+function randomClass(grupoClasse) {
   const index = Math.ceil(Math.random() * grupoClasse.length) - 1;
   return grupoClasse[index];
 }
 
 function addRamdomClasses(element) {
-  const classesStyle = [`newspaper`, `magazine1`, `magazine2`];
-  const classesTamano = [`medium`, `big`, `reallybig`];
-  const classesRotateInclinate = [`rotateleft`, `rotateright`, `skewleft`, `skewright`];
+  const classesStyle = ['newspaper', 'magazine1', 'magazine2'];
+  const classesTamano = ['medium', 'big', 'reallybig'];
+  const classesRotateInclinate = ['rotateleft', 'rotateright', 'skewleft', 'skewright'];
 
   element.classList.add(randomClass(classesStyle));
   element.classList.add(randomClass(classesTamano));
@@ -20,25 +21,30 @@ function addRamdomClasses(element) {
 
 function deleteExistentSpans() {
   while (text.firstChild) {
-    text.removeChild(text.lastChild)
+    text.removeChild(text.lastChild);
   }
 }
 
+function atualizaContador() {
+  contador = text.children.length;
+  contadorOutput.innerHTML = contador;
+}
+
 function createLetter() {
-  if (inputText.value === '' || inputText.value === ' ') {
-    text.innerText = 'Por favor, digite o conteúdo da carta.'
-  }
-  else if (text.children.length > 0) {
-    deleteExistentSpans()
-  }else {
-    const inputValue = inputText.value.split(' ')
+  if (inputText.value.trim() === '') {
+    text.innerText = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    text.innerText = '';
+    deleteExistentSpans();
+    const inputValue = inputText.value.split(' ');
     for (let i = 0; i < inputValue.length; i += 1) {
-      let spanTag = document.createElement('span');
+      const spanTag = document.createElement('span');
       addRamdomClasses(spanTag);
       text.appendChild(spanTag);
       spanTag.innerText = inputValue[i];
     }
+    atualizaContador();
   }
 }
 
-createLetterBtn.addEventListener('click', createLetter)
+createLetterBtn.addEventListener('click', createLetter);
