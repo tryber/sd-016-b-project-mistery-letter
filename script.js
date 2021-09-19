f332a0f48736c655c8207d7c52120daa0c81f2b4
Juliane Cardoso <julianesilvac75@gmail.com>
2021-09-19 17:22:39 -0300

    requisito 5 concluido

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ let generateBtn = document.getElementById('criar-carta');
 function generateLetter() {
   let inputLetter = document.getElementById('carta-texto').value;
   let splitInput = inputLetter.split(' ');
-  document.getElementById('carta-gerada').innerText = '';
+  let letter = document.getElementById('carta-gerada');
+  letter.innerText = '';
 
   for (let index = 0; index < splitInput.length; index += 1) {
-    let newWord = document.createElement('span');
-    newWord.innerText = splitInput[index];
-    document.getElementById('carta-gerada').appendChild(newWord);
+    if (splitInput[index] === '') {
+      letter.innerText = 'Por favor, digite o conteúdo da carta.';
+    } else {
+      let newWord = document.createElement('span');
+      newWord.innerText = splitInput[index];
+      document.getElementById('carta-gerada').appendChild(newWord);
+    }
   }
-
 }
 
 generateBtn.addEventListener('click', generateLetter);
