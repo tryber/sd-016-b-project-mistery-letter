651b7cc49e813815f1c33316bd88856055ec14e2
Juliane Cardoso <julianesilvac75@gmail.com>
2021-09-19 19:55:53 -0300

    corrige erros lint

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-let letter = document.getElementById('carta-gerada');
-let generateBtn = document.getElementById('criar-carta');
+const letter = document.getElementById('carta-gerada');
+const generateBtn = document.getElementById('criar-carta');
 
-let style = ['newspaper', 'magazine1', 'magazine2'];
-let size = ['medium', 'big', 'reallybig'];
-let rotation = ['rotateleft', 'rotateright'];
-let skew = ['skewleft', 'skewright'];
+const style = ['newspaper', 'magazine1', 'magazine2'];
+const size = ['medium', 'big', 'reallybig'];
+const rotation = ['rotateleft', 'rotateright'];
+const skew = ['skewleft', 'skewright'];
 
 function addClasses(newElement) {
-  let class1 = style[Math.round(Math.random() * 2)];
-  let class2 = size[Math.round(Math.random() * 2)];
-  let class3 = rotation[Math.round(Math.random() * 1)];
-  let class4 = skew[Math.round(Math.random() * 1)];
+  const el = newElement;
+  const class1 = style[Math.round(Math.random() * 2)];
+  const class2 = size[Math.round(Math.random() * 2)];
+  const class3 = rotation[Math.round(Math.random() * 1)];
+  const class4 = skew[Math.round(Math.random() * 1)];
 
-  newElement.className = `${class1} ${class2} ${class3} ${class4}`;
+  el.className = `${class1} ${class2} ${class3} ${class4}`;
 }
 
 function generateLetter() {
-  let inputLetter = document.getElementById('carta-texto').value;
-  let splitInput = inputLetter.split(' ');
+  const inputLetter = document.getElementById('carta-texto').value;
+  const splitInput = inputLetter.split(' ');
   letter.innerText = '';
 
   for (let index = 0; index < splitInput.length; index += 1) {
     if (splitInput[index] === '') {
       letter.innerText = 'Por favor, digite o conteúdo da carta.';
     } else {
-      let newWord = document.createElement('span');
+      const newWord = document.createElement('span');
       document.getElementById('carta-gerada').appendChild(newWord);
       addClasses(newWord);
       newWord.innerText = splitInput[index];
     }
   }
 
-  let wordCount = letter.children;
-  document.getElementById('carta-contador').innerText = wordCount.length
+  const wordCount = letter.children;
+  document.getElementById('carta-contador').innerText = wordCount.length;
 }
 
 generateBtn.addEventListener('click', generateLetter);
