b758c70ed8349ad1452da585486d6b7e28c81a0d
Juliane Cardoso <julianesilvac75@gmail.com>
2021-09-19 17:14:59 -0300

    requisito 3 concluido

---
+++ script.js
@@ -0,0 +1,18 @@
+let generateBtn = document.getElementById('criar-carta');
+
+function generateLetter() {
+  let inputLetter = document.getElementById('carta-texto').value;
+  let splitInput = inputLetter.split(' ');
+  document.getElementById('carta-gerada').innerText = '';
+
+  for (let index = 0; index < splitInput.length; index += 1) {
+    let newWord = document.createElement('span');
+    newWord.innerText = splitInput[index];
+    document.getElementById('carta-gerada').appendChild(newWord);
+  }
+
+  document.getElementById('carta-texto').value = '';
+
+}
+
+generateBtn.addEventListener('click', generateLetter);
