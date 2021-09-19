6f3a32f1ff5e9f88d497f8503cf0e610f6a6a937
Wellington <tinhoefaela@gmail.com>
2021-09-19 11:54:46 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
 const input = document.querySelector('#carta-texto');
 const button = document.querySelector('#criar-carta');
+const contador = document.querySelector('#carta-contador');
+
+// Estilos para classe do elemento span gerado ao botão gerar carta misteriosa
 const estilo = ['newspaper', 'magazine1', 'magazine2'];
 const tamanho = ['medium', 'big', 'reallybig'];
 const rotacao = ['rotateleft', 'rotateright'];
 const inclinação = ['skewleft', 'skewright'];
 
-button.addEventListener('click', () => {
+function GenerateCard() {
+  button.addEventListener('click', () => {
+    const p = document.querySelector('#carta-gerada');
+    const span = document.createElement('span');
+    if (input.value === '' || input.value === ' ') {
+      p.innerHTML = '';
+      span.innerText = 'Por favor, digite o conteúdo da carta.';
+      p.appendChild(span);
+    } else {
+      p.innerHTML = '';
+      span.innerHTML = input.value;
+      span.className = estilo[Math.floor(Math.random() * estilo.length)] + ' ' + tamanho[Math.floor(Math.random() * tamanho.length)] + ' ' + rotacao[Math.floor(Math.random() * rotacao.length)] + ' ' + inclinação[Math.floor(Math.random() * inclinação.length)];
+      p.appendChild(span);
+    }
+  });
+}
+function changeClassOnClick() {
   const p = document.querySelector('#carta-gerada');
-  const span = document.createElement('span');
-  if (input.value === '' || input.value === ' ') {
-    span.innerText = "Por favor, digite o conteúdo da carta.";
-    p.appendChild(span);
-  } else {
-    span.innerHTML = input.value;
-    span.classList.add(estilo[Math.floor(Math.random() * estilo.length)], tamanho[Math.floor(Math.random() * tamanho.length)], rotacao[Math.floor(Math.random() * rotacao.length)], inclinação[Math.floor(Math.random() * inclinação.length)]);
-    p.appendChild(span);
-  }
-});
+  p.addEventListener('click', (e) => {
+    e.target.className = estilo[Math.floor(Math.random() * estilo.length)] + ' ' + tamanho[Math.floor(Math.random() * tamanho.length)] + ' ' + rotacao[Math.floor(Math.random() * rotacao.length)] + ' ' + inclinação[Math.floor(Math.random() * inclinação.length)];
+  });
+}
+
+function count() {
+  button.addEventListener('click', () => {
+    const text = input.value;
+    const split = text.split(' ').length;
+    contador.innerHTML = split;
+  });
+}
+
+window.onload = () => {
+  changeClassOnClick();
+  GenerateCard();
+  count();
+}
