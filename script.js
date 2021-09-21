e74ce563f02e9858abb48b40033fed47affcc5c1
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2021-09-21 04:26:40 -0300

    Requisitos até o 17 - Ok

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 const buttonCreateText = document.getElementById('criar-carta');
 const textArea = document.getElementById('carta-gerada');
 
+const classStyle = ['newspaper', 'magazine1', 'magazine2'];
+const classSize = ['medium', 'big', 'reallybig'];
+const classRotation = ['rotateright', 'rotateleft'];
+const classInclination = ['skewleft', 'skewright'];
+function randomClasseStyle() {
+  const classeS = `${classStyle[parseInt(Math.random() * classStyle.length)]} `;
+  return classeS;
+}
+
+function randomClasseSize() {
+  const classeS = `${classSize[parseInt(Math.random() * classSize.length)]} `;
+  return classeS;
+}
+
+function randomClasseRotation() {
+  const classeS = `${classRotation[parseInt(Math.random() * classRotation.length)]} `;
+  return classeS;
+}
+
+function randomClasseInclination() {
+  const classeS = `${classInclination[parseInt(Math.random() * classInclination.length)]}`;
+  return classeS;
+}
+
+// Função para checar se o input está vazio ou se é apeas expaço em branco.
 function checkInput() {
   const inputText = document.getElementById('carta-texto').value;
   let countSpaces = 0;
@@ -14,6 +39,7 @@ function checkInput() {
   }
 }
 
+// Evento botão para adicionar o texto
 buttonCreateText.addEventListener('click', () => {
   const inputText = document.getElementById('carta-texto').value;
   textArea.innerText = '';
@@ -21,8 +47,13 @@ buttonCreateText.addEventListener('click', () => {
   if (textArea.innerText === '') {
     const textoQuebrado = inputText.split(' ');
     for (let index = 0; index < textoQuebrado.length; index += 1) {
+      randomClasseStyle();
       const spanText = document.createElement('span');
-      spanText.innerText = ` ${textoQuebrado[index]} `;
+      spanText.className = randomClasseStyle();
+      spanText.className += randomClasseSize();
+      spanText.className += randomClasseRotation();
+      spanText.className += randomClasseInclination();
+      spanText.innerText = `${textoQuebrado[index]} `;
       textArea.appendChild(spanText);
     }
   }
