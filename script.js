186417fbd6b082c462e93a8f0fc30f05e8d929b8
Millene Galetti <mikagaletti1@gmail.com>
2021-10-07 18:36:07 -0300

    Requisito 16

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const button = document.querySelector('#criar-carta');
 const inputText = document.querySelector('#carta-texto');
 const paragraphText = document.querySelector('#carta-gerada');
+const style = ['newspaper', 'magazine1', 'magazine2'];
+const size = ['medium', 'big', 'reallybig'];
+const rotate = ['rotateleft', 'rotateright'];
+const skew = ['skewleft', 'skewright'];
+
+function randomGroup(group) {
+  const position = Math.ceil(Math.random() * group.length) - 1;
+  return group[position];
+}
+// Math.ceil - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/ceil
+// https://www.w3bai.com/pt/jsref/jsref_ceil.html
+
+function addClassGroup(span) {
+  span.classList.add(randomGroup(style));
+  span.classList.add(randomGroup(size));
+  span.classList.add(randomGroup(rotate));
+  span.classList.add(randomGroup(skew));
+}
 
 function letter() {
   paragraphText.innerHTML = '';
@@ -11,9 +29,14 @@ function letter() {
     splitWords.forEach((word) => {
       const createSpan = document.createElement('span');
       createSpan.innerText = word;
+      addClassGroup(createSpan);
       paragraphText.append(createSpan);
     });
   }
 }
 
 button.addEventListener('click', letter);
+
+// * Trim -Para verificar se o value de um input está vazio ou com vários espaços em branco.
+// https://pt.stackoverflow.com/questions/58498/se-input-tiver-apenas-espa%C3%A7os-em-branco-n%C3%A3o-fazer-nada
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/trim.
