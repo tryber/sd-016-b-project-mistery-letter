b24c2cd2cc61726fe73ecae7257eff9e56c30ac7
[Gabriel Rodrigues Leite] <k.cogabriel@gmail.com>
2021-10-10 22:19:23 -0300

    requisito 5 ok

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-console.log('ok');
+function gerarCarta() {
+  let texto = document.querySelector('#carta-texto').value;
+  // https://stackoverflow.com/questions/10261986/how-to-detect-string-which-contains-only-spaces
+  if (!texto.length || !texto.replace(/\s/g, '').length) {
+    texto = 'Por favor, digite o conteúdo da carta.';
+  }
+  document.querySelector('#carta-gerada').innerText = texto;
+}
 
+function configurar() {
+  document.querySelector('#criar-carta').addEventListener('click', gerarCarta);
+}
+
+window.onload = configurar;
 // nova linha pra agradar o linter
